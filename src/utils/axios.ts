eab6a830e9fe5567c82cc7777b0f73e2ed771ff4
HyreKaro <ssanjit856@gmail.com>
2024-06-07 20:29:17 +0530

    added env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Adjust according to your backend server
+  baseURL: import.meta.env.VITE_API_URL, // Adjust according to your backend server
 });
 
 api.interceptors.request.use(
